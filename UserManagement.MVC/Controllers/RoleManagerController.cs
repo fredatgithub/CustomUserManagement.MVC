80b9c578bf82fe3fd7e9db402959f660ad1b488a
fred <domaine75@gmail.com>
2023-08-17 09:15:44 +0200

    clean code

--- UserManagement.MVC/Controllers/RoleManagerController.cs
+++ UserManagement.MVC/Controllers/RoleManagerController.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace UserManagement.MVC.Controllers
 {
-    public class RoleManagerController : Controller
+  public class RoleManagerController : Controller
+  {
+    private readonly RoleManager<IdentityRole> _roleManager;
+    public RoleManagerController(RoleManager<IdentityRole> roleManager)
     {
-        private readonly RoleManager<IdentityRole> _roleManager;
-        public RoleManagerController(RoleManager<IdentityRole> roleManager)
-        {
-            _roleManager = roleManager;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var roles = await _roleManager.Roles.ToListAsync();
-            return View(roles);
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddRole(string roleName)
-        {
-            if (roleName != null)
-            {
-                await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
-            }
-            return RedirectToAction("Index");
-        }
+      _roleManager = roleManager;
     }
+    public async Task<IActionResult> Index()
+    {
+      var roles = await _roleManager.Roles.ToListAsync();
+      return View(roles);
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddRole(string roleName)
+    {
+      if (roleName != null)
+      {
+        await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+      }
+      return RedirectToAction("Index");
+    }
+  }
 }
--- UserManagement.MVC/Enums/Roles.cs
+++ UserManagement.MVC/Enums/Roles.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace UserManagement.MVC.Enums
+﻿namespace UserManagement.MVC.Enums
 {
-    public enum Roles
-    {
-        SuperAdmin,
-        Admin,
-        Moderator,
-        Basic
-    }
+  public enum Roles
+  {
+    SuperAdmin,
+    Admin,
+    Moderator,
+    Basic
+  }
 }
